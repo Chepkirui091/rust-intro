164168fe7f78e337640d8e8bf8cf10b7f007b84a
Chepkirui91 <chepkiruidaphne91@gmail.com>
2024-08-26 16:53:55 +0300

    if conditions loops files

---
+++ practical/branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/daphne/Desktop/Rust/practical/branches/target/debug/branches: /home/daphne/Desktop/Rust/practical/branches/src/main.rs
---
+++ practical/branches/target/debug/deps/branches-c67bf3d6b1525430.d
@@ -0,0 +1,5 @@
+/home/daphne/Desktop/Rust/practical/branches/target/debug/deps/branches-c67bf3d6b1525430: src/main.rs
+
+/home/daphne/Desktop/Rust/practical/branches/target/debug/deps/branches-c67bf3d6b1525430.d: src/main.rs
+
+src/main.rs:
