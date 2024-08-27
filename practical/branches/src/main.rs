713ffdf77fd93316dbfbf2c39c5a5ec33b04c462
Chepkirui91 <chepkiruidaphne91@gmail.com>
2024-08-27 12:14:11 +0300

    loop with for

--- practical/branches/src/main.rs
+++ practical/branches/src/main.rs
@@ -129,6 +129,15 @@ fn main() {
         }
     }
 
+    {
+        let a = [5; 10];
+        let mut sum = 0;
+        for x in a {
+            sum += x;
+        }
+        println!("{sum}");
+    }
+
 
 }
 
