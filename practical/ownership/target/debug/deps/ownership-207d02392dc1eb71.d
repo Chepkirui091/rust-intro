b942173d78d52a34b61d6aa421ebcf28c7bef9c2
Chepkirui91 <chepkiruidaphne91@gmail.com>
2024-09-17 15:58:10 +0300

    ownership unversioned files

---
+++ practical/ownership/target/debug/deps/ownership-207d02392dc1eb71.d
@@ -0,0 +1,5 @@
+/home/daphne/Desktop/Rust/practical/ownership/target/debug/deps/ownership-207d02392dc1eb71: src/main.rs
+
+/home/daphne/Desktop/Rust/practical/ownership/target/debug/deps/ownership-207d02392dc1eb71.d: src/main.rs
+
+src/main.rs:
